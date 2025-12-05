886992557cec14be9b53aa71cb7f396f9af3c997
Mohammad Faheem <faheemmd169@gmail.com>
2025-12-05 20:46:28 +0530

    Lecture as on 05/12/2025 01:54:40

---
+++ chai.py
@@ -0,0 +1,5 @@
+from hello import chai
+
+chai("ginger tea")
+
+# This is a comment
--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+def chai(n):
+    print(n)
+
+chai("lemon tea")
+
+chai_one  = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala tea"
